064eef28f8bfa679593e9156c203bb9b46931b82
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-07-15 13:40:21 +0500

    Add files via upload

---
+++ Hello-agent/main.py
@@ -0,0 +1,10 @@
+from agents import Agent , Runner
+
+agent= Agent(
+    name = "Assistant",
+    instructions= "are you halpfull assistant"
+)
+
+result = Runner.run_sync(agent , "Holle agent")
+
+print (result.final_output)
