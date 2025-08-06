c9512e75ca01021d055b035bcb4f7826023a7578
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-08-06 23:10:14 +0500

    Update main.py

--- Hello-agent/main.py
+++ Hello-agent/main.py
@@ -1,9 +1,9 @@
 from agents import Agent , Runner
-
 agent= Agent(
     name = "Assistant",
     instructions= "are you halpfull assistant"
 )
 result = Runner.run_sync(agent , "Holle agent")
 print (result.final_output)
+
 
