8f66bc448bf5b0c8baa0c841a1111c5da66c5c27
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-08-11 10:17:18 +0500

    Update main.py

--- Assignments/Assignment-02/main.py
+++ Assignments/Assignment-02/main.py
@@ -1,6 +1,6 @@
 import asyncio
 from dotenv import load_dotenv
-from agents import Agent , Runner , function_tool , OpenAIChatCompletionsModel , AsyncOpenAI ,RunContextWrapper
+from agents import Agent , Runner , function_tool , OpenAIChatCompletionsModel , AsyncOpenAI ,RunContextWrapper , input_guardrail , GuardrailFunctionOutput
 import os 
 from agents.run import RunConfig
 from pydantic import BaseModel
@@ -26,7 +26,25 @@ class Account(BaseModel):
     name : str
     pin: int
 
-def check_user(ctx:RunContextWrapper[Account],agent:Agent) -> bool:
+
+class My_output():
+    name : str
+    balance : str 
+
+guardrail_agent = Agent(
+    name = "Guardrail Agent ",
+    instructions= "You are a guardrail agent . You check if the user is asking you bank related questions. ",
+    output_type= My_output
+)
+
+@input_guardrail 
+async def check_bank_related(ctx : RunContextWrapper [None], agent:Agent, input:str)-> GuardrailFunctionOutput:
+
+
+
+
+
+def check_user(ctx:RunContextWrapper[Account],agent:Agent)-> bool:
     if ctx.context.name == "Bisma" and ctx.context.pin == 5678:
         return True
     else:
@@ -42,15 +60,16 @@ def check_balance(account_number :str ) -> str:
 bank_agent = Agent(
     name = "Bank_Agent",
     instructions= "You are  a bank  agent. You help customers with theri questions Your can use the tools to get the .",
-    tools=[check_balance]
+    tools=[check_balance],
+    output_type= My_output
 
 )
 
 user_context = Account(name="Bisma", pin= 5678)
-propmt =input( "Hello ma Bankig Agent! What can a help you:\n ")
+# propmt =input( "Hello ma Bankig Agent! What can a help you:\n ")
 
 def main():
-    result = Runner.run_sync(bank_agent , propmt , context=user_context, run_config=config )
+    result = Runner.run_sync(bank_agent , "I want to my balance my account on is 309473804" , context=user_context, run_config=config )
     print(result.final_output)
 
 if __name__ == "__main__" :
